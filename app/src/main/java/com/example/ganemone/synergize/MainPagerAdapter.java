87aa6c27e1e82f3980050412f897dd3ff30e3d62
Giancarlo Anemone <ganemone@gmail.com>
2015-03-29 13:18:17 -0400

    Initial commit

---
+++ app/src/main/java/com/example/ganemone/synergize/MainPagerAdapter.java
@@ -0,0 +1,32 @@
+package com.example.ganemone.synergize;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.view.View;
+
+/**
+ * Created by ganemone on 3/19/15.
+ */
+public class MainPagerAdapter extends FragmentPagerAdapter {
+
+    public MainPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return TestFragment.newInstance(position);
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return "ARGGGG TITLE";
+    }
+}
