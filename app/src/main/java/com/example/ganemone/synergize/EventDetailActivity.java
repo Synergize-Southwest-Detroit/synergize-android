bc0c61a800d75458e1210def308cc3f52f5977f7
Giancarlo Anemone <ganemone@gmail.com>
2015-03-31 15:12:01 -0400

    Add initial implementation of event detail view controller

---
+++ app/src/main/java/com/example/ganemone/synergize/EventDetailActivity.java
@@ -0,0 +1,47 @@
+package com.example.ganemone.synergize;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class EventDetailActivity extends ActionBarActivity {
+
+    public Event event;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_detail);
+        Intent intent = getIntent();
+        int eid = intent.getIntExtra(EventsFragment.EVENT_EXTRA, -1);
+        if (eid == -1) {
+            // SOMETHING WENT WRONG!!!
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_event_detail, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/com/example/ganemone/synergize/EventsFragment.java
+++ app/src/main/java/com/example/ganemone/synergize/EventsFragment.java
@@ -1,11 +1,14 @@
 package com.example.ganemone.synergize;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -14,8 +17,9 @@ import java.util.Date;
 /**
  * Created by ganemone on 3/29/15.
  */
-public class EventsFragment extends Fragment {
+public class EventsFragment extends Fragment implements ListView.OnItemClickListener {
 
+    public final static String EVENT_EXTRA = "com.synergize.synergize.EVENT_EXTRA";
     EventListAdapter adapter;
     ListView listView;
 
@@ -36,6 +40,15 @@ public class EventsFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.events_fragment, container, false);
         listView = (ListView) rootView.findViewById(R.id.event_list_view);
         listView.setAdapter(adapter);
+        listView.setOnItemClickListener(this);
         return rootView;
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Intent intent = new Intent(getActivity(), EventDetailActivity.class);
+        Date now = new Date();
+        intent.putExtra(EVENT_EXTRA, 1);
+        startActivity(intent);
     }
 }
